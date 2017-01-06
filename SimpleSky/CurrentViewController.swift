6b5b4d302215a4012291a98caa15354b50baee0b
David Wang <david.wang34@gmail.com>
2017-01-05 23:39:48 -0500

    first commit

---
+++ SimpleSky/CurrentViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSky
+//
+//  Created by David Wang on 1/5/17.
+//  Copyright © 2017 David Wang. All rights reserved.
+//
+
+import UIKit
+
+class CurrentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
